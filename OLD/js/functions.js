54878463700561ccd05ca69ae63f3135bc0867c6
Hendrik Johannes Otto Van Heerden <20916892@sun.ac.za>
2021-03-21 12:43:51 +0000

    update

---
+++ OLD/js/functions.js
@@ -0,0 +1,8 @@
+function getIP(){
+    fetch("https://api.ipify.org/?format=json")
+        .then(results => results.json())
+        .then((json) => {
+            return document.getElementById("ip_address").innerHTML = json.ip;
+        })
+}
+
