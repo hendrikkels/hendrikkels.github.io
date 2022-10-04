1edad3caa33e719de23b4aa641f10a3fb5a847f4
Hendrik van Heerden <hendrikkels@icloud.com>
2022-10-04 16:50:11 +0200

    added new word

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(event){
     // array with texts to type in typewriter
-    var dataText = [ "Developer", "Systems Architect", "Software Engineer",];
+    var dataText = [ "Developer", "Systems Architect", "Legend", "Software Engineer",];
     
     // type one text in the typwriter
     // keeps calling itself until the text is finished
