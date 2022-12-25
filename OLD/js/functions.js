53e32f35229826381390b508d16552cedc4ae5ea
Hendrik van Heerden <hendrikkels@icloud.com>
2022-12-25 19:36:41 +0200

    removed old folder

--- OLD/js/functions.js
+++
@@ -1,8 +0,0 @@
-function getIP(){
-    fetch("https://api.ipify.org/?format=json")
-        .then(results => results.json())
-        .then((json) => {
-            return document.getElementById("ip_address").innerHTML = json.ip;
-        })
-}
-
